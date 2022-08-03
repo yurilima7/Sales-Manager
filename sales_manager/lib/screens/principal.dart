2602056c0d64c8cd43efdce599f89e71935c2c6d
Yuri S. Lima <61335084+Yuri-S-Lima@users.noreply.github.com>
2022-08-03 17:07:48 -0300

    Remoção do FutureBuilder

--- sales_manager/lib/screens/principal.dart
+++ sales_manager/lib/screens/principal.dart
@@ -42,11 +42,11 @@ class _PrincipalState extends State<Principal> {
     });
   } 
 
-   _recebeVendas() async{
+  _recebeVendas() async{
     List vendas = [];
 
     await db.collection("Usuários").doc(usuarioID.toString())
-    .collection("Últimas Vendas").get().then((querySnapshot) => {
+    .collection("Últimas Vendas").orderBy("Id", descending: true).get().then((querySnapshot) => {
         for (var doc in querySnapshot.docs){
           vendas.add(doc.data()),
         },
@@ -54,9 +54,10 @@ class _PrincipalState extends State<Principal> {
 
     setState(() {
       _ultimasVendas.add(vendas);
-       
     });
-   }
+
+    print(_ultimasVendas);
+  }
 
   final _vendas = [
     {'Nome': "Fulano", 'Produto': "Cinto", 'valor': 20},
@@ -65,178 +66,160 @@ class _PrincipalState extends State<Principal> {
     {'Nome': "Marciano", 'Produto': "Camisa", 'valor': 60},
   ];
 
-  Future<String> getNome() async =>
-    await Future.delayed(const Duration(seconds: 2), () {
-      return _nomeUsuario;
-  });
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
 
-      body: FutureBuilder(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         
-        future: getNome(),
-        builder: (context, snapshot){
-
-          if(snapshot.hasData){
-            return Padding(
-              padding: const EdgeInsets.all(20.0),
-              
-              child: CustomScrollView(
-                physics: const BouncingScrollPhysics(), // remove o sombreamento da scroll
-          
-                slivers: [
-          
-                  SliverFillRemaining(
-                    hasScrollBody: false,
-          
-                    child: Column(
+        child: CustomScrollView(
+          physics: const BouncingScrollPhysics(), // remove o sombreamento da scroll
+    
+          slivers: [
+    
+            SliverFillRemaining(
+              hasScrollBody: false,
+    
+              child: Column(
+            
+                children: [
                   
-                      children: [
-                        
-                        Expanded( // nome de usuário e perfil
-                          flex: 1,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            
-                            children: [
-                              Text(snapshot.data.toString(), style: const TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold)),
-                        
-                              Ink(
-                                
-                                decoration: const BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.white,
-                        
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.grey,
-                                      blurRadius: 5
-                                    )
-                                  ]
-                                ),
-                        
-                                width: MediaQuery.of(context).size.width * 0.11,
-                        
-                                child: IconButton(
-                                  onPressed: () => {Navigator.pushNamed(context, '/editarPerfil')}, 
-                              
-                                  icon: SvgPicture.asset(
-                                    'assets/icons/pessoa.svg',
-                                    height: MediaQuery.of(context).size.height * 0.025,
-                                    width: MediaQuery.of(context).size.width * 0.025,
-                                    allowDrawingOutsideViewBox: true,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        
-                        Expanded(
-                          flex: 4,
-                          child: Column(
-                            children: [
-                              SizedBox(height: MediaQuery.of(context).size.height * 0.04),
-                              // Card de informações de vendas
-                              Card( 
-                                color: const Color(0xFF6D3F8C),
-                                elevation: 3,
-                                
-                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                        
-                                child: Padding(
-                                  padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.04),
-                        
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        
-                                    children: [
-                                      Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        
-                                        children: const [
-                                          Text("Vendido", style: TextStyle(color: Colors.white, fontSize: 16)),
-                                          SizedBox(height: 2),
-                                          Text("R\$ 200", style: TextStyle(color: Colors.white, fontSize: 16)),
-                                        ],
-                                      ),
-                        
-                                      SvgPicture.asset(
-                                        'assets/icons/carteira.svg',
-                                        height: MediaQuery.of(context).size.height * 0.04,
-                                        width: MediaQuery.of(context).size.width * 0.04,
-                                        allowDrawingOutsideViewBox: true,
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                        
-                              SizedBox(height: MediaQuery.of(context).size.height * 0.035),
-                              // Botões de ações do aplicativo
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: const [
-                        
-                                  BotaoAcesso(caminho: 'assets/icons/vendas.svg', titulo: "Adicionar Vendas", proximo: '/opcaoDeVenda'),
-                                  BotaoAcesso(caminho: 'assets/icons/add pagamento.svg', titulo: "Adicionar Pagamentos", proximo: '/addPagamento'),
-                                  BotaoAcesso(caminho: 'assets/icons/clientes.svg', titulo: "Consultar Clientes", proximo: '/clientes'),
-                                  
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                        
-                        Expanded(
-                          flex: 4,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                        
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: const [
-                                  Text("Últimas vendas", style: TextStyle(color: Color(0xFF6D3F8C), fontWeight: FontWeight.bold, fontSize: 16)),
-                                  
-                                ],
-                              ),
-                              // Listagem das vendas mais recentes do usuário
-                              Column(
-                              
-                                children: [
-                                  ListaRecentes(cliente: _vendas[0].values.elementAt(0).toString(), produto: _vendas[0].values.elementAt(1).toString(), valor: double.parse(_vendas[0].values.elementAt(2).toString())),
-                                  SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-                                  ListaRecentes(cliente: _vendas[1].values.elementAt(0).toString(), produto: _vendas[1].values.elementAt(1).toString(), valor: double.parse(_vendas[1].values.elementAt(2).toString())),
-                                  SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-                                  ListaRecentes(cliente: _vendas[2].values.elementAt(0).toString(), produto: _vendas[2].values.elementAt(1).toString(), valor: double.parse(_vendas[2].values.elementAt(2).toString())),
-                                  SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-                                  ListaRecentes(cliente: _vendas[3].values.elementAt(0).toString(), produto: _vendas[3].values.elementAt(1).toString(), valor: double.parse(_vendas[3].values.elementAt(2).toString())),
-                                ],
-                              ),
-                            ],
-                          ), 
-                        ),
-                        // Card de navegação no aplicativo
-                        const CardInferior(ativo: true)
+                  Expanded( // nome de usuário e perfil
+                    flex: 1,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       
+                      children: [
+                        Text(_nomeUsuario, style: const TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold)),
+                  
+                        Ink(
+                          
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
+                  
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey,
+                                blurRadius: 5
+                              )
+                            ]
+                          ),
+                  
+                          width: MediaQuery.of(context).size.width * 0.11,
+                  
+                          child: IconButton(
+                            onPressed: () => {Navigator.pushNamed(context, '/editarPerfil')}, 
+                        
+                            icon: SvgPicture.asset(
+                              'assets/icons/pessoa.svg',
+                              height: MediaQuery.of(context).size.height * 0.025,
+                              width: MediaQuery.of(context).size.width * 0.025,
+                              allowDrawingOutsideViewBox: true,
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                   ),
+                  
+                  Expanded(
+                    flex: 4,
+                    child: Column(
+                      children: [
+                        SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+                        // Card de informações de vendas
+                        Card( 
+                          color: const Color(0xFF6D3F8C),
+                          elevation: 3,
+                          
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                  
+                          child: Padding(
+                            padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.04),
+                  
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  
+                                  children: const [
+                                    Text("Vendido", style: TextStyle(color: Colors.white, fontSize: 16)),
+                                    SizedBox(height: 2),
+                                    Text("R\$ 200", style: TextStyle(color: Colors.white, fontSize: 16)),
+                                  ],
+                                ),
+                  
+                                SvgPicture.asset(
+                                  'assets/icons/carteira.svg',
+                                  height: MediaQuery.of(context).size.height * 0.04,
+                                  width: MediaQuery.of(context).size.width * 0.04,
+                                  allowDrawingOutsideViewBox: true,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                  
+                        SizedBox(height: MediaQuery.of(context).size.height * 0.035),
+                        // Botões de ações do aplicativo
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: const [
+                  
+                            BotaoAcesso(caminho: 'assets/icons/vendas.svg', titulo: "Adicionar Vendas", proximo: '/opcaoDeVenda'),
+                            BotaoAcesso(caminho: 'assets/icons/add pagamento.svg', titulo: "Adicionar Pagamentos", proximo: '/addPagamento'),
+                            BotaoAcesso(caminho: 'assets/icons/clientes.svg', titulo: "Consultar Clientes", proximo: '/clientes'),
+                            
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  
+                  Expanded(
+                    flex: 4,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                  
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: const [
+                            Text("Últimas vendas", style: TextStyle(color: Color(0xFF6D3F8C), fontWeight: FontWeight.bold, fontSize: 16)),
+                            
+                          ],
+                        ),
+                        // Listagem das vendas mais recentes do usuário
+                        Column(
+                        
+                          children: [
+                            ListaRecentes(cliente: _vendas[0].values.elementAt(0).toString(), produto: _vendas[0].values.elementAt(1).toString(), valor: double.parse(_vendas[0].values.elementAt(2).toString())),
+                            SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                            ListaRecentes(cliente: _vendas[1].values.elementAt(0).toString(), produto: _vendas[1].values.elementAt(1).toString(), valor: double.parse(_vendas[1].values.elementAt(2).toString())),
+                            SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                            ListaRecentes(cliente: _vendas[2].values.elementAt(0).toString(), produto: _vendas[2].values.elementAt(1).toString(), valor: double.parse(_vendas[2].values.elementAt(2).toString())),
+                            SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                            ListaRecentes(cliente: _vendas[3].values.elementAt(0).toString(), produto: _vendas[3].values.elementAt(1).toString(), valor: double.parse(_vendas[3].values.elementAt(2).toString())),
+                          ],
+                        ),
+                      ],
+                    ), 
+                  ),
+                  // Card de navegação no aplicativo
+                  const CardInferior(ativo: true)
+                
                 ],
               ),
-            );
-          }
-          else{
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        }
-      ),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
